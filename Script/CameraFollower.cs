657619c39ebcbfa5d8aa05227686e379add8342b
Henno Théo <zoupeurletroll@gmail.com>
2023-09-24 12:06:54 +0200

    smoothed cam update

--- Script/CameraFollower.cs
+++ Script/CameraFollower.cs
@@ -1,21 +1,11 @@
 using Godot;
-using System;
 
 public partial class CameraFollower : Node2D
 {
     [Export] private Node2D _objectToFollow = null;
-    [Export] private float _maxSpeed = 6f;
-    [Export] private float _maxSpeedDistance = 50f;
 
     public override void _Process(double delta)
     {
-        Vector2 deltaPos = _objectToFollow.Position - Position;
-
-        if (deltaPos.Length() < 1f)
-            return;
-
-        Vector2 clampedDelta = deltaPos.LimitLength(_maxSpeedDistance) / _maxSpeedDistance;
-
-        Position += _maxSpeed * (float)delta * clampedDelta;
+        Position = _objectToFollow.Position;
     }
 }
