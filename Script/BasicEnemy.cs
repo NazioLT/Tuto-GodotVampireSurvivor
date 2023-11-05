5f0d24805596eb732e4d351632f9c90bf680cbdf
Henno Théo <94365544+NazioLT@users.noreply.github.com>
2023-11-05 13:37:35 +0100

    flip and refacto

--- Script/BasicEnemy.cs
+++ Script/BasicEnemy.cs
@@ -3,14 +3,13 @@ using Godot;
 public partial class BasicEnemy : Node
 {
 	[Export] private CharacterMotor _motor = null;
-	[Export] private Player _player = null;
 
 	[Export] private float _attackDistance = 1f;
 	[Export] private float _attackDamagePerSecond = 10f;
 
 	public override void _Process(double delta)
 	{
-		Vector2 deltaPlayerPosition = _player.BodyPosition - _motor.GlobalPosition;
+		Vector2 deltaPlayerPosition = Player.Instance.BodyPosition - _motor.GlobalPosition;
 		float distanceToPlayer = deltaPlayerPosition.Length();
 
         if (distanceToPlayer < _attackDistance)
@@ -26,7 +25,7 @@ public partial class BasicEnemy : Node
 	{
 		_motor.Stop();
 
-		_player.Life.Damage(_attackDamagePerSecond * (float)delta);
+        Player.Instance.Life.Damage(_attackDamagePerSecond * (float)delta);
     }
 
     private void GoToPlayer(Vector2 deltaPlayerPosition)
--- Script/CharacterAnimator.cs
+++ Script/CharacterAnimator.cs
@@ -1,23 +1,30 @@
 using Godot;
 using System;
 
-public partial class CharacterAnimator : AnimationPlayer
+public partial class CharacterAnimator : Node
 {
-    [Export] private Node2D _characterSprite = null;
+    [Export] private Sprite2D _characterSprite = null;
+    [Export] private AnimatedSprite2D _animatedCharacterSprite = null;
+    [Export] private AnimationPlayer _animationPlayer = null;
     [Export] private CharacterMotor _motor = null;
 
     public const string ENEMY_ANIM = "EnemyAnim";
-    
+
     public override void _Ready()
     {
         base._Ready();
-        
-        CurrentAnimation = ENEMY_ANIM;
+
+        if (_animationPlayer != null)
+            _animationPlayer.CurrentAnimation = ENEMY_ANIM;
     }
 
     public override void _Process(double delta)
     {
         bool goLeft = _motor.Velocity.X > 0;
-        _characterSprite.RotationDegrees = 0;
+        if (_characterSprite != null)
+            _characterSprite.FlipH = goLeft;
+
+        if (_animatedCharacterSprite != null)
+            _animatedCharacterSprite.FlipH = goLeft;
     }
 }
